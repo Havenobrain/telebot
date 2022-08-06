c831467d3f2783efa640c88bf7275319bb2f19f0
Havenobrain <107768179+Havenobrain@users.noreply.github.com>
2022-08-06 15:59:11 +0300

    Initial commit

---
+++ dog.py
@@ -0,0 +1,6 @@
+TOKEN = "15554949565:AAHTlrLcNjGi2eMQC5r2Dm5-ScuKEL6IOBY"
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
---
+++ main.py
@@ -0,0 +1,54 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+import telebot
+from cat import APIException, Convertor
+from dog import TOKEN, exchanges
+import traceback
+
+exchanges = {
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'рубль': 'RUB'
+}
+TOKEN = "5554949565:AAHTlrLcNjGi2eMQC5r2Dm5-ScuKEL6IOBY"
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def start(message: telebot.types.Message):
+    text = "Приветствие!"
+    bot.send_message(message.chat.id, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for i in exchanges.keys():
+        text = '\n'.join((text, i))
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text'])
+def converter(message: telebot.types.Message):
+    values = message.text.split(' ')
+    try:
+        if len(values) != 3:
+            raise APIException('Неверное количество параметров!')
+
+        answer = Convertor.get_price(*values)
+    except APIException as e:
+        bot.reply_to(message, f"Ошибка в команде:\n{e}")
+    except Exception as e:
+        traceback.print_tb(e.__traceback__)
+        bot.reply_to(message, f"Неизвестная ошибка:\n{e}")
+    else:
+        bot.reply_to(message, answer)
+
+
+bot.polling()
+
